042294a034cce43705fcf7b434b7081435a992f9
raulportela <raul.p.p@hotmail.com>
2023-08-22 21:56:38 -0300

    card slides

---
+++ siteGrupoInovar/js/script.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 6,
+    spaceBetween: 25,
+    slidesPerGroup: 4,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: false,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
